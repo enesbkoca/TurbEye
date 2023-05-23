1590cd0bd7cc1a1fc8d36a56783da1720aaf05f8
berkay <enesbkoca@gmail.com>
2023-05-23 15:41:33 +0200

    Changes by arham

--- tests/test_propeller.py
+++ tests/test_propeller.py
@@ -14,8 +14,20 @@ class PropTest(unittest.TestCase):
         self.assertTrue(self.P.Bp == 2)
         self.assertTrue(self.P.name == "Dummy")
         self.assertTrue(self.P.mass == 0.1)
+        self.assertTrue(self.P.__repr__() == 'Dummy')
 
     def test_coefficients(self):
-        self.assertTrue(np.isclose(self.P.Ct == 0.04399))
-        self.assertTrue(self.P.Cm == 0.003578)
-        self.assertTrue(self.P.Cd == 0.019334)
+        self.assertTrue(np.isclose(self.P.Ct, 0.04399, rtol=0.01, atol=0.001))
+        self.assertTrue(np.isclose(self.P.Cm, 0.003578, rtol=0.01, atol=0.001))
+        self.assertTrue(np.isclose(self.P.Cd, 0.019334, rtol=0.01, atol=0.001), msg=self.P.Cd)
+
+    def test_forces(self):
+        T, M = self.P.forces(600)
+        self.assertTrue(np.isclose(T, 0.04399*1.225*100*0.5**4, rtol=0.01, atol=0.001), msg=T)
+        self.assertTrue(np.isclose(M, 0.003578 * 1.225 * 100 * 0.5 ** 5, rtol=0.01, atol=0.001), msg=M)
+
+    def test_req_rpm(self):
+        N = self.P.required_rpm(1)
+        self.assertTrue(np.isclose(N, (1/0.04399/1.225/(0.5**4))**0.5*60, rtol=0.01, atol=0.001), msg=N)
+
+
