4012075a2f61949202d8d06a25818f4c98097fb4
MArhElahi <arhamsaba@gmail.com>
2023-05-26 12:26:27 +0200

    Small changes, turning into csv etc

--- src/drone_combinator.py
+++ src/drone_combinator.py
@@ -56,6 +56,7 @@ class DroneCombinator:
                 )
             )
 
+        self.table = table
         print(table)
 
     def __getitem__(self, item):
@@ -65,3 +66,4 @@ class DroneCombinator:
 if __name__ == "__main__":
     combinations = DroneCombinator()
     combinations.print_drones(count=20, upper_limit=15)
+
